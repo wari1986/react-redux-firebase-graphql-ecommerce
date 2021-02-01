1c7af34023381ef1ffff9bc9168bdda82bb53a6e
wari1986 <nicolaycamacho@gmail.com>
2021-02-01 17:52:09 -0400

    firebase folder and configuration added

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCzS5odegFCRqGLNLKYWCyybFJxtBQn2V4",
+    authDomain: "pistacho-ecommerce-website.firebaseapp.com",
+    projectId: "pistacho-ecommerce-website",
+    storageBucket: "pistacho-ecommerce-website.appspot.com",
+    messagingSenderId: "869119859743",
+    appId: "1:869119859743:web:4119ce8f2e8c4e2ccc092a",
+    measurementId: "G-QDMZZ8JDFS"
+  };
